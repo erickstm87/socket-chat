528c355501c8156835c3be5e6e7a2ad7363aae4b
Thomas Erickson <terickson@spotxchange.com>
2017-02-16 13:22:58 -0700

    added io.emit function

--- server/server.js
+++ server/server.js
@@ -13,20 +13,13 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected');
-  socket.emit('newEmail', {
-    from: 'tommy@example.com',
-    text: 'this is the text of the email',
-    createdAt: 123
-  });
-
-  socket.emit('newMessage', {
-    from: 'someone',
-    message: 'someone created a message',
-    createdAt: 12312
-  });
 
   socket.on('message', (message) => {
-    console.log('the client\'s message was emitted', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {

e3ad0c42840da1363deb3d72687a5550aa161e43
Thomas Erickson <terickson@spotxchange.com>
2017-02-25 08:41:22 -0700

    incorporated jquery so no more dev tools

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ var socket = io();
 socket.on('connect', function() {
   console.log('connected to server');
 
-  socket.emit('message', {
+  socket.emit('createdMessage', {
     from: 'tomas',
     message: "here is the client's message"
   });
@@ -15,11 +15,26 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
 });
 
 socket.emit('createMessage', {
   from: 'tomas',
   text: 'some more text'
-}, function(){
-  console.log('got it');
+}, function(serverData){
+  console.log('got it', serverData);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
 });
--- server/server.js
+++ server/server.js
@@ -13,27 +13,28 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('new user connected');
+  console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome welcome'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit(('newMessage'), generateMessage('Admin', 'New user joined'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('message', (message, callback) => {
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback();
+    callback('This is from the server');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
-    // })
+    // });
   });
 
   socket.on('disconnect', () => {
-    console.log('disconnected');
-  })
+    console.log('User was disconnected');
+  });
 });
 
 server.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+  console.log(`Server is up on ${port}`);
 });

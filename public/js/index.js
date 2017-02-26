91dc2575f9e3ccdf20477429072eb0bd023334d1
Thomas Erickson <terickson@spotxchange.com>
2017-02-25 21:55:13 -0700

    autoscroll feature added

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom(){
+  //Selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', function() {
   console.log('connected to server');
 
@@ -23,6 +39,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -41,6 +58,7 @@ socket.on('newLocationMessage', function(message) {
     url: message.url
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 // socket.emit('createMessage', {
